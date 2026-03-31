62476cd41c58e269f817a56619e5bf769a986edc
We Are Go City <wearegocity@Wes-Mac-mini-389.local>
2026-03-31 15:38:18 +0200

    new directors

--- src/components/Directors.jsx
+++ src/components/Directors.jsx
@@ -47,29 +47,29 @@ export default function Directors() {
                     <DirectorCard
                         image="./images/Festus.jpeg"
                         name="Dr. Festus Maelo Sawoni"
-                        position="Director / Chairperson"
+                        position="President"
                         bio="Holds BCom, MBA, and PhD in Developmental Economics. Responsible for contracts, training, and international affairs. He brings extensive experience in economic development strategy, regional trade coordination, and institutional leadership across multiple African markets."
                     />
 
                     <DirectorCard
                         image="./images/Joe.jpeg"
                         name="Joe Visagie"
-                        position="Director / Chairman"
+                        position="Vice President"
                         bio="As a dedicated Director on the Chamber of Commerce, I bring a strong foundation in administration and operations, driving strategic growth and organizational efficiency. With a proven ability to streamline processes, manage complex initiatives, and lead cross-functional teams, I am committed to advancing the Chamber’s mission while ensuring operational excellence. My focus on governance, compliance, and member service ensures that our programs and partnerships are delivered with integrity, impact, and sustainability. Passionate about local business development, I work collaboratively with stakeholders to foster a vibrant, inclusive, and thriving business community."
                     />
 
-                    {/* <DirectorCard
-                        image="./images/Siwa2.png"
-                        name="Siwapiwe Mpondonkulu"
-                        position="Finance Director"
-                        bio="Siwapiwe is a finance professional with a background in Investment Management. She holds ACMA and CGMA designations from the Chartered Institute of Management Accountants (CIMA) and contributes strong financial governance and strategic investment oversight."
-                    /> */}
+                    <DirectorCard
+                        image="./images/anou.png"
+                        name="Dr S.M Mambo"
+                        position="Director(Technology & Innovation)"
+                        bio=""
+                    />
 
                     <DirectorCard
-                        image="./images/shadrack.jpeg"
-                        name="Nomsa Dlamini"
-                        position="Director of Technology & Innovation"
-                        bio="Holds MSc in Radio & TV Engineering (Odessa), DTech in Electrical Engineering (TUT), and PhD (UPEC). Professional Engineer (Kenya), MIEK, SMIEEE, and IEEE CASS Ambassador for Africa 2024-2025. Leading innovation, digital transformation, and technology development."
+                        image="./images/anou.png"
+                        name="Mr Mogodi Matamela"
+                        position="Director/CFO"
+                        bio=""
                     />
 
                 </div>
